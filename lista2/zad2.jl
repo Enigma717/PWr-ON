d41f457ccce45bef64275c2437bc9b057424d122
Enigma717 <mt88x2@gmail.com>
2022-11-20 12:49:24 +0100

    Improved plot

--- lista2/zad2.jl
+++ lista2/zad2.jl
@@ -7,13 +7,16 @@ using Plots
 
 
 function main()
-    func(x)::Float64 = exp(x) * log(1.0 + exp(-x))
+    func(x)::Float64 = exp(Float64(x)) * log(Float64(1.0) + exp(Float64(-x)))
 
     funcplot = plot(func,
                     title = "f(x) = e^x * ln(1 + e^-x)",
                     label = "f(x)",
                     xlims = (0.0, 50.0),
-                    ylims = (0.0, 2.0))
+                    xticks = 0.0:5.0:50.0,
+                    ylims = (0.0, 2.0),
+                    linecolor = :red,
+                    lw = 2)
 
     Plots.png(funcplot, "zad2.png")
 end
